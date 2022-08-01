d9b4cadf149ba08c65f9e918baa0f71ce8e6a5f2
Xu Zhi <68148230+Toxicfireball@users.noreply.github.com>
2022-08-01 15:13:00 +0800

    Update models.py

--- appsec/models.py
+++ appsec/models.py
@@ -86,6 +86,7 @@ class graph(db.Model):
     __tablename__ = 'graph'
 
     id = db.Column(db.Integer, primary_key=True)
+    gra = db.Columen(db.Boolean, nullable = True)
     DATE1 = db.Column(db.String(30), nullable=True)
     DATE2 = db.Column(db.String(30), nullable=True)
     DATE3 = db.Column(db.String(30), nullable=True)
