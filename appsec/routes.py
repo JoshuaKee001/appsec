43b38d32e1c6b10d38d106f19851416a52d4ff19
JoshuaKee001 <joshuakeezuen2004@gmail.com>
2022-08-15 00:56:01 +0800

    more integration

--- appsec/routes.py
+++ appsec/routes.py
@@ -587,8 +587,9 @@ def staffprod():
     products = Product.query.all()
     data = []
     for product in products:
-        new = (product.name, product.no_sold)
-        data.append(new)
+        if product.no_sold > 0:
+            new = (product.name, product.no_sold)
+            data.append(new)
 
     labels = [row[0] for row in data]
     values = [row[1] for row in data]
@@ -1735,7 +1736,7 @@ def delete_feedback(user_id):
       db.session.delete(feed)
       id = feed.id
       db.session.commit()
-      return redirect(url_for('retrieveConsultation' ))
+      return redirect(url_for('retrievefeedback' ))
 
      else:
          return redirect(url_for('login'))
@@ -1750,7 +1751,7 @@ def feedform():
       if form.validate_on_submit():
           user = current_user
 
-          tday = str(datetime.today())
+          tday = str(datetime.date.today())
           excluded_chars = "*?!'^+%&/()=}][{$#"
           appointment = False
 
@@ -1758,43 +1759,17 @@ def feedform():
           sub = form.subject.data.lower()
           descrip = form.description.data.lower()
 
+          FEform = feedback(
+              username=user.username,
+              date=tday,
+              email=form.email.data.lower(),
+              subject=form.subject.data.lower(),
+              description=form.description.data.lower())
 
+          db.session.add(FEform)
+          db.session.commit()
 
-
-
-          if excluded_chars in ema:
-                appointment = False
-                raise ValidationError
-
-          else:
-                appintment = True
-          if excluded_chars in sub:
-                appointment = False
-                raise ValidationError
-
-          else:
-                appointment = False
-          if excluded_chars in descrip:
-                appointment = False
-                raise ValidationError
-
-          else:
-                appointment = False
-
-          if appointment == True:
-
-
-              FEform = feedback(
-              username = user.username,
-              date = tday ,
-              email = form.email.data.lower(),
-              subject = form.subject.data.lower(),
-              description = form.description.data.lower())
-
-              db.session.add(FEform)
-              db.session.commit()
-
-              return redirect(url_for('fb_submit'))
+          return redirect(url_for('fb_submit'))
       else:
         return render_template('user/guest/alisa/feedback.html', usersession = True, form = form)
 
